6f3c5d7b484291c19e0294d6a5a92015c54171cc
dev-mkr <webdev.mu@gmail.com>
2026-04-01 22:26:42 +0200

    fix: update redirect path in home component to point to custom themes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Redirect } from "@docusaurus/router";
 
 function Home() {
-  return <Redirect to="/docs/category/themes" />;
+  return <Redirect to="/docs/category/custom-themes" />;
 }
 
 export default Home;
